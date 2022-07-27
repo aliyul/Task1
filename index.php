5f8e5ea764cfb4e8486e64e93171fa4817ceea50
Ali-yul <waliprogrammer@gmail.com>
2022-07-27 07:24:27 +0700

    comitted

--- index.php
+++ index.php
@@ -1,19 +1,29 @@
 
+<html> 
 <head>
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+	<title>Subsequence PHP</title>
 </head>
+<body>
+<h1>Input on the text box what ever you want for Subsequence result </h1>
 <form name="form" action="" method="POST">
-<input id="myinputbox" name="myinputbox" type="text" value="" autofocus onkeydown="search(this)"/>
-<input id="myinputbox1" name="myinputbox1" type="text" value="" onkeydown="search(this)"/>
-</form>
+<!--<input id="myinputbox" name="myinputbox" type="text" value="" autofocus onkeydown="search(this)"/>
+<input id="myinputbox1" name="myinputbox1" type="text" value="" onkeydown="search(this)"/>-->
+<input id="myinputbox" name="myinputbox" type="text" value="" autofocus onkeydown="search()"/>
+<input id="myinputbox1" name="myinputbox1" type="text" value="" onkeydown="search()"/>
 
+</form>
+</body>
+</html>
 
 <script>
 	window.onload = function() {
   var input = document.getElementById("myinputbox").focus();
 }
 
-function search(ele) {
+//function search(ele) {
+function search() {
+
     if(event.key === 'Enter') {
     // alert(ele.value);        
 		   jQuery.ajax({
